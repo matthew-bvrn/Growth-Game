e8e3f294b769998820a58495dfa870c1e5aa4b4e
Matthew B <foorganders@outlook.com>
2022-09-06 10:19:53 +0200

    Use unity delta time to make time passing independent of breakpoints

--- Assets/Scripts/Plant/PlantManagerRealtime.cs
+++ Assets/Scripts/Plant/PlantManagerRealtime.cs
@@ -5,14 +5,11 @@ using UnityEngine;
 
 public partial class PlantManagerRealtime : PlantManagerBase
 {
-	double m_lastSimulationTime;
-
 	//TODO move somewhere sensible like game manager
-	public static float m_testDeltaMultiplier = 2000;
+	public static float m_testDeltaMultiplier = 500;
 
 	public void Start()
 	{
-		m_lastSimulationTime = GetTime();
 		GameConsole.Instance.AddCommand("simulateall", DebugSimulate);
 		GameConsole.Instance.AddCommand("simulatenearest", DebugSimulateNearest);
 		GameConsole.Instance.AddCommand("waterall", DebugWaterAll);
@@ -23,24 +20,16 @@ public partial class PlantManagerRealtime : PlantManagerBase
 	//TODO temporary, this should eventually be put into a game state manager
 	public void Update()
 	{
-		Simulate();
+		Simulate(Time.deltaTime);
 	}
 
-	void Simulate()
+	void Simulate(float delta)
 	{
-		double time = GetTime();
-		float delta = (float)(time - m_lastSimulationTime) * m_testDeltaMultiplier;
+		delta *= m_testDeltaMultiplier;
 
 		foreach (PlantComponent plant in GetComponentsInChildren<PlantComponent>())
 		{
 			plant.GetComponent<GrowthComponent>().Simulate(delta);
 		}
-
-		m_lastSimulationTime = time;
-	}
-
-	double GetTime()
-	{
-		return DateTime.Now.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
 	}
 }
