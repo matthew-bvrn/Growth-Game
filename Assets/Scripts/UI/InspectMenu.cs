71b12caba2e8e9f3d4e7a3c9248b07756963a44d
Matthew B <foorganders@outlook.com>
2022-11-26 15:27:35 +0100

    Add arrows to plant panel sliders

--- Assets/Scripts/UI/InspectMenu.cs
+++ Assets/Scripts/UI/InspectMenu.cs
@@ -9,6 +9,11 @@ public class InspectMenu : MonoBehaviour
 	[SerializeField] Slider m_healthSlider;
 	[SerializeField] Slider m_waterSlider;
 
+	[SerializeField] RawImage m_waterUp;
+	[SerializeField] RawImage m_waterDown;
+	[SerializeField] RawImage m_healthUp;
+	[SerializeField] RawImage m_healthDown;
+
 	public void OnInspect()
 	{
 		StateManager.Get.TrySetState(EGameState.Inspecting);
@@ -35,10 +40,41 @@ public class InspectMenu : MonoBehaviour
 	{
 		if(StateManager.Get.State == EGameState.Inspecting)
 		{
-			Debug.Log(SelectablesManager.Get.Selected.GetComponent<Parameters.ParametersComponent>().WaterHealth);
-			m_healthSlider.value = SelectablesManager.Get.Selected.GetComponent<Parameters.ParametersComponent>().GrowthFactor;
-			m_waterSlider.value = SelectablesManager.Get.Selected.GetComponent<Parameters.ParametersComponent>().WaterLevel;
+			float health = SelectablesManager.Get.Selected.GetComponent<Parameters.ParametersComponent>().GrowthFactor;
+			float water = SelectablesManager.Get.Selected.GetComponent<Parameters.ParametersComponent>().WaterLevel;
+
+			UpdateArrows(m_waterSlider.value, water, m_waterUp, m_waterDown);
+			UpdateArrows(m_healthSlider.value, health, m_healthUp, m_healthDown);
+
+			m_healthSlider.value = health;
+			m_waterSlider.value = water;
+
+
 		}
+	}
+
+	void UpdateArrows(float oldVal, float newVal, RawImage up, RawImage down)
+	{
+		if (newVal > oldVal)
+		{
+			up.gameObject.SetActive(true);
+			down.gameObject.SetActive(false);
+		}
+		else if(newVal < oldVal)
+		{
+			up.gameObject.SetActive(false);
+			down.gameObject.SetActive(true);
+		}
+		else
+		{
+			up.gameObject.SetActive(false);
+			down.gameObject.SetActive(false);
+		}
+	}
+
+	public void OnWaterPressed()
+	{
+		SelectablesManager.Get.Selected.GetComponent<GrowthComponent>().Water();
 	}
 
 }
