73078ce9c0900bc9edd00afcb8b60cb0083f83db
Matthew B <foorganders@outlook.com>
2022-12-11 15:56:52 +0100

    Make money counter count down slowly

--- Assets/Scripts/UI/CoinCounter.cs
+++ Assets/Scripts/UI/CoinCounter.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class CoinCounter : MonoBehaviour
 {
-    // Update is called once per frame
-    void Update()
-    {
-		GetComponent<TMPro.TMP_Text>().text = InventoryManager.Get.Money.ToString();       
-    }
+	int m_value = 0;
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (m_value != InventoryManager.Get.Money)
+		{
+			int diff = InventoryManager.Get.Money - m_value;
+
+			m_value += (int)Mathf.Ceil(diff / 30f);
+
+			if (Mathf.Abs(diff) < 30)
+				m_value = InventoryManager.Get.Money;
+		}
+		GetComponent<TMPro.TMP_Text>().text = m_value.ToString();
+	}
 }
